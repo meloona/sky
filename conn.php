980e93755cd9399f76ed2ac325979aff161ac07b
meloona <33996552+meloona@users.noreply.github.com>
2020-01-05 15:29:55 +0400

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
         ob_start();
-		$host="den1.mysql3.gear.host"; // Host name 
+		$host="mysql3.gear.host"; // Host name 
 		$username="dbSky"; // Mysql username 
 		$password="Uo6fzhg-t3D_"; // Mysql password 
 		$db_name="dbSky"; // Database name 
